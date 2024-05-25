be6df23c95e770a0d0bf6ca8c2a349a62ee0e18d
TadeasKaba <tadeas.kaba@gmail.com>
2024-05-25 11:46:43 +0200

    filter added

--- modules/Inventory.js
+++ modules/Inventory.js
@@ -6,12 +6,13 @@ class Inventory
 
     static #GET_AVAILABLE_ITEMS = "GetAvailableItems";
 
-    static async getAvailableItems(category = "ANY", max = -1, offset = 0)
+    static async getAvailableItems(category = "ANY", max = -1, offset = 0, filter = {})
     {
         var items = await APIController.Get(this.#MODULE, this.#GET_AVAILABLE_ITEMS, {
             'category': category,
             'max': max,
             'offset': offset,
+            'filter': JSON.stringify(filter),
         });
 
         return items;
