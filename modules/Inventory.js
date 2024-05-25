e8dbab66a3b00dd175dea47910a8ef832073f79d
TadeasKaba <tadeas.kaba@gmail.com>
2024-05-25 15:33:47 +0200

    new api

--- modules/Inventory.js
+++ modules/Inventory.js
@@ -5,6 +5,16 @@ class Inventory
     static #MODULE = "Inventory";
 
     static #GET_AVAILABLE_ITEMS = "GetAvailableItems";
+    static #SALE_COUPON_EXISTS = "SaleCouponExists";
+
+    static async saleCouponExists(code)
+    {
+        var exists = await APIController.Get(this.#MODULE, this.#SALE_COUPON_EXISTS, {
+            'code': code,
+        });
+
+        return exists;
+    }
 
     static async getAvailableItems(category = "ANY", max = -1, offset = 0, filter = {})
     {
