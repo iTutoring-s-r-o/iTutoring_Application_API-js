6ddb242429aa5b9ec1cea1f26205af8f510f98c4
TadeasKaba <tadeas.kaba@gmail.com>
2024-07-07 11:46:30 +0200

    update

--- apiController.js
+++ apiController.js
@@ -109,7 +109,7 @@ class APIController
                 if (APIController.onConfimationReceived !== null && APIController.onConfimationReceived !== undefined)
                 {
                     APIController.ConfirmationToken = json.data['conf_request']['token'];
-                    json.data["confirm"] = this.ConfirmRequest;
+                    json.data['conf_request']["confirm"] = this.ConfirmRequest;
                     APIController.onConfimationReceived(json.data['conf_request']);
                 }
             }
