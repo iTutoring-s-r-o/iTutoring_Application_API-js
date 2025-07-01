00ed1f96237a5b1f0d7fa4e6d44e69c277d94fe3
TadeasKaba <tadeas.kaba@gmail.com>
2025-07-01 15:53:59 +0200

    api update

--- modules/CustomerAuth.js
+++ modules/CustomerAuth.js
@@ -18,6 +18,16 @@ class CustomerAuth
     // All method names
     static #SIGN_IN = "SignIn";
     static #RESEND_CONFIRMATION_EMAIL = 'ResendConfirmationEmail';
+    static #REQUEST_PASSWORD_CHANGE = 'RequestPasswordChange';
+
+    static async RequestPasswordChange(email)
+    {
+        var result = await APIController.Get(this.#MODULE, this.#REQUEST_PASSWORD_CHANGE, {
+            'email': email
+        });
+
+        return result;
+    }
 
     static async ResendConfirmationEmail(email)
     {
@@ -32,18 +42,13 @@ class CustomerAuth
      * 
      * @param {*} email 
      * @param {*} pass 
-     * @param {*} fname 
-     * @param {*} lname 
      * @returns Int (AuthResult)
      */
-    static async SignIn(email, pass, fname, lname, phone)
+    static async SignIn(email, pass)
     {
         var result = await APIController.Post(this.#MODULE, this.#SIGN_IN, {
             'email': email,
             'pass': pass,
-            'fname': fname,
-            'lname': lname,
-            'phone': phone,
         });
 
         return result;
